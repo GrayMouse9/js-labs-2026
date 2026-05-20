4e1ec22efb6f436d69309d48989f4b6509ab947d
GrayMouse9 <zenacederbaum@gmail.com>
2026-05-20 23:47:22 +0300

    LR5

--- example-express/src/index.js
+++ example-express/src/index.js
@@ -13,16 +13,6 @@ trajectoriesService.init(DATA_FILE_PATH);
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
-app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
